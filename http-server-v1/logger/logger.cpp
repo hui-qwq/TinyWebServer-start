73c8d057df82f7bd74ba9b8b2b162eed906e4040
hui-qwq <2475504826@qq.com>
2026-04-29 10:02:26 -0700

    更新异步日志

--- http-server-v1/logger/logger.cpp
+++ http-server-v1/logger/logger.cpp
@@ -8,6 +8,7 @@
 #include <mutex>
 #include <sstream>
 #include <system_error>
+#include <thread>
 
 
 Logger& Logger::instance() {
@@ -15,30 +16,102 @@ Logger& Logger::instance() {
     return logger;
 }
 
+Logger::~Logger() {
+    shutdown();
+}
 
-void Logger::init(const std::string& log_dir, bool to_stdout) {
-    std::lock_guard<std::mutex> lock(mutex_);
-
-    log_dir_ = log_dir;
-    to_stdout_ = to_stdout;
-
-    std::error_code ec;
-    std::filesystem::create_directories(log_dir, ec);
-    if (ec) {
-        std::cerr << "[LOGGER][ERROR] create_directories failed: " << ec.message() << '\n';
+void Logger::shutdown() {
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if(stopping_) {
+            return ;
+        }
+        stopping_ = true;
     }
-    open_log_file_locked(now_date_str());
-    inited_ = true;
+
+    cv_.notify_all();
+
+    if(worker_.joinable()) {
+        worker_.join();
+    }
+}
+void Logger::worker_loop() {
+    while(true) {
+        Task task_;
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            cv_.wait(lock, [this]() {
+                return dq_.size() || stopping_;
+            });
+
+            if(dq_.empty() && stopping_) return ;
+
+            task_ = std::move(dq_.front()); 
+            dq_.pop_front();
+        }
+        task_();
+    }
+}
+
+void Logger::init(const std::string& log_dir, bool to_stdout)  {
+
+    bool need_start = false;
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+
+        log_dir_ = log_dir;
+        to_stdout_ = to_stdout;
+        stopping_ = false;
+
+        std::error_code ec;
+        std::filesystem::create_directories(log_dir, ec);
+        if (ec) {
+            std::cerr << "[LOGGER][ERROR] create_directories failed: " << ec.message() << '\n';
+        }
+        open_log_file_locked(now_date_str());
+
+        inited_ = true;
+
+        need_start = !worker_.joinable();
+    }
+
+    if (need_start) {
+        worker_ = std::thread([this]() {
+            worker_loop();
+        });
+    }
 }
 
 
-void Logger::info(const std::string& msg) { log(LogLevel::INFO, msg); }
-void Logger::warn(const std::string& msg) { log(LogLevel::WARN, msg); }
-void Logger::error(const std::string& msg) { log(LogLevel::ERROR, msg); }
+void Logger::info(const std::string& msg) { 
+    enqueue([this, msg]() {
+        log(LogLevel::INFO, msg);
+    });
+}
+void Logger::warn(const std::string& msg) {
+    enqueue([this, msg]() {
+        log(LogLevel::WARN, msg); 
+    });
+}
+void Logger::error(const std::string& msg) {
+    enqueue([this, msg]() {
+        log(LogLevel::ERROR, msg);
+    });
+}
 
+void Logger::enqueue(Task task) {
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if(stopping_ || dq_.size() >= max_tasks_) {
+            return ;
+        }
+        dq_.push_back(std::move(task));
+    }
+
+    cv_.notify_one();
+}
 
 void Logger::log(LogLevel level, const std::string& msg) {
-    std::lock_guard<std::mutex> lock(mutex_);
 
     if (!inited_) {
         log_dir_ = "logs";
--- http-server-v1/logger/logger.hpp
+++ http-server-v1/logger/logger.hpp
@@ -1,36 +1,48 @@
 #pragma once
 
+#include <condition_variable>
 #include <fstream>
 #include <mutex>
 #include <string>
-
+#include <functional>
+#include <deque>
+#include <thread>
 enum class LogLevel { INFO, WARN, ERROR };
 
 class Logger {
-public:
-    static Logger& instance();
-
-    void init(const std::string& log_dir = "logs", bool to_stdout = true);
-    void info(const std::string& msg);
-    void warn(const std::string& msg);
-    void error(const std::string& msg);
-
 private:
-    Logger() = default;
-    Logger(const Logger&) = delete;
-    Logger& operator=(const Logger&) = delete;
+    using Task = std::function<void()>;
+    size_t max_tasks_ = 10000;
+    std::deque<Task> dq_;
+    std::thread worker_;
+    std::condition_variable cv_;
 
-    void log(LogLevel level, const std::string& msg);
-    void open_log_file_locked(const std::string& date_str);
-    std::string now_time_str() const;
-    std::string now_date_str() const;
-    std::string level_str(LogLevel level) const;
-
-private:
     std::mutex mutex_;
     std::ofstream file_;
     std::string log_dir_;
     std::string cur_date_;
     bool to_stdout_ = true;
     bool inited_ = false;
+    bool stopping_ = false;
+public:
+    static Logger& instance();
+    void init(const std::string& log_dir = "logs", bool to_stdout = true);
+    void info(const std::string& msg);
+    void warn(const std::string& msg);
+    void error(const std::string& msg);
+    void enqueue(Task task);
+private:
+    Logger() = default;
+    ~Logger();
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    void log(LogLevel level, const std::string& msg);
+    void open_log_file_locked(const std::string& date_str);
+    void worker_loop();
+    void shutdown();
+    std::string now_time_str() const;
+    std::string now_date_str() const;
+    std::string level_str(LogLevel level) const;
 };
+
